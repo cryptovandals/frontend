327558b6fa223a2558193873fd1b075228c8326c
buyonegetonefree <d024d2f523973e30f2c9c898e52a803e@protonmail.com>
2018-11-08 01:02:44 +0100

    add vandalizeme contract

---
+++ contracts/VandalizeMe.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.18;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
+
+contract VandalizeMe is Ownable, ERC721Token {
+  string[] public works;
+
+  constructor (string _name, string _symbol) public
+    ERC721Token(_name, _symbol)
+  {
+  }
+
+  function mint(
+    address _to,
+    string  _tokenURI
+  ) external
+  {
+    uint workId = works.push(_tokenURI) - 1;
+    super._mint(_to, workId);
+    super._setTokenURI(workId, _tokenURI);
+  }
+}
