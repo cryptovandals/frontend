6906e4817fbc82ac1a97152801ef1a6db65b5306
Buy One Get One Free <d024d2f523973e30f2c9c898e52a803e@protonmail.com>
2018-11-13 11:02:26 +0100

    Add remix history

--- contracts/CryptoVandals.sol
+++ contracts/CryptoVandals.sol
@@ -1,11 +1,19 @@
 pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Basic.sol";
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
-import "./ITransferContract.sol";
 
 contract CryptoVandals is Ownable, ERC721Token {
-  string[] public works;
+
+  struct Remix {
+    address contract1;
+    uint256 tokenId1;
+    address contract2;
+    uint256 tokenId2;
+  }
+
+  mapping(uint256 => Remix) public remixes;
 
   constructor (string _name, string _symbol) public
     ERC721Token(_name, _symbol)
@@ -13,16 +21,38 @@ contract CryptoVandals is Ownable, ERC721Token {
   }
 
   function mint(
-    address _to,
-    address _source,
-    string  _tokenURI,
-    uint256 _tokenId
+    address _owner,
+
+    address _sourceContract1,
+    uint256 _sourceTokenId1,
+
+    address _sourceContract2,
+    uint256 _sourceTokenId2,
+
+    string  _newTokenURI
   ) external
   {
-    ITransferContract sourceContract = ITransferContract(_source);
-    sourceContract.transferFrom(_to, address(1), _tokenId);
-    uint workId = works.push(_tokenURI) - 1;
-    super._mint(_to, workId);
-    super._setTokenURI(workId, _tokenURI);
+    require(_sourceContract1.isContract());
+    require(_sourceContract2 == address(0) || _sourceContract2.isContract());
+
+    ERC721Basic(_sourceContract1).transferFrom(_owner, address(1), _sourceTokenId1);
+
+    if (_sourceContract2 != address(0)) {
+      ERC721Basic(_sourceContract2).transferFrom(_owner, address(1), _sourceTokenId2);
+    }
+
+    uint newTokenId = totalSupply().add(1);
+
+    Remix memory remix = Remix({
+      contract1: _sourceContract1,
+      tokenId1: _sourceTokenId1,
+      contract2: _sourceContract2,
+      tokenId2: _sourceTokenId2
+    });
+
+    remixes[newTokenId] = remix;
+
+    super._mint(_owner, newTokenId);
+    super._setTokenURI(newTokenId, _newTokenURI);
   }
 }
--- contracts/ITransferContract.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.4.24;
-
-contract ITransferContract {
-    function transferFrom(address _from, address _to, uint256 _tokenId) external;
-}
