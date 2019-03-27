255aec2d327cb340f88a54da35ea968672fe8894
Buy One Get One Free <44848611+buyonegetonefree@users.noreply.github.com>
2019-03-27 14:15:27 +0100

    Update doc (#11)
    * Refactor contract and tests
    
    * Add smart contract documentation
    
    * Taste the rainbow
    
    Added good things. Took out bad things.

--- contracts/CryptoVandals.sol
+++ contracts/CryptoVandals.sol
@@ -13,16 +13,55 @@
  *         ##   "mm"#  #   #  "#m##  "mm"#    "mm  "mmm"
  */
 
+// # Welcome to the CryptoVandals Smart Contract
+//
+// **Warning: Using this contract will destroy your NFT (and generate a new
+// one). Don't use it if you don't know what you are doing.**
+//
+// The logic of the contract is simple. The contract exposes two
+// user friendly functions:
+// - `vandalize(address _contract, uint256 _tokenId, string _newTokenURI)`;
+// - ```vandalize2(
+//  address _contract1,
+//  uint256 _tokenId1,
+//  address _contract2,
+//  uint256 _tokenId2,
+//  string _newTokenURI)```;
+//
+// `vandalize` calls the `transferFrom` function (defined in the ERC721
+// standard) to *burn* the original NFT. In CryptoVandals, burning the 
+// NFT means transferring it from its original owner 
+// to the address `0x1` in the original contract.
+// After a successful transfer, the CryptoVandals contract mints a new NFT
+// (adding the metadata pointed by `_newTokenURI`) and assigns it to
+// `msg.sender`. A new record in the `sources` is then added. 
+// The record contains the provenance of the NFT: the original 
+// contract address and token ID.
+//
+// `vandalize2` works just like `vandalize`, but allows you to combine two NFTs
+// into a single, vandalized NFT. Magical! 
+// Use this function if you want to do something fancy, like adding an audio 
+// track to a Cryptokitty. 
+//
+// You can also re-vandalize a NFT you've already vandalized. If you do this, 
+// the old NFT is burned, a new NFT is minted, and the `sources` is updated 
+// with a list (or tree, if you use `vandalize2`) of parent NFTs.
+//
+// Important: before calling any vandalize function, you **must** call
+// `approve(address to, uint256 tokenId)` in the contract where your NFT is
+// currently stored. If you don't do that, CryptoVandals won't be able to burn
+// your NFT to generate a new one.
 
 
 pragma solidity >=0.4.21 <0.6.0;
 
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
+import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Burnable.sol';
 
 
-// We use OpenZeppelin ERC-721 as a baseline for our contract.
-contract CryptoVandals is ERC721Full, ERC721Mintable {
+// We use the OpenZeppelin ERC-721 standard as a baseline for CryptoVandals.
+contract CryptoVandals is ERC721Full, ERC721Mintable, ERC721Burnable {
 
   // Every new NFT token minted by this contract has one or two sources,
   // stored in the struct `Source`.
